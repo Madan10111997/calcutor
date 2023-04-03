8db8e8254d8044f774b547ff8af9e13d6be8ed95
Madan10111997 <129039011+Madan10111997@users.noreply.github.com>
2023-04-03 13:08:11 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -8,8 +8,8 @@ def multiply(a, b):
     return a * b
 
 # Get user input
-num1 = int(input("Enter first number:10 "))
-num2 = int(input("Enter second number:20 "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 # Perform calculations
 result_add = add(num1, num2)
